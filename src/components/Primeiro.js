f5f4934159a28fda72b0d9768a1ef7149495ad7d
Rodrigo Gossi <rowtigo@gmail.com>
2021-11-14 15:02:39 -0300

    console warn

--- src/components/Primeiro.js
+++ src/components/Primeiro.js
@@ -4,5 +4,7 @@ import { Text } from 'react-native';
 import Estilo from './style'
 
 export default () => {
+
+    console.warn("opa!") //gera um toats visual 
     return <Text style={Estilo.fontG}>Meu primeiro componente!</Text>
 }
